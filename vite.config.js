a5b15d4f8ec7f8c2c3838cb98f115d2ec2580f45
Leroy-jester <josefmelcher@gmail.com>
2026-06-21 00:08:14 -0400

    alterações para o pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+        includeAssets: [
+        "bg-hero-campanhas.jpg",
+        "dnd.jpg",
+        "Ordem.jpg",
+        "paperTexture.jpg",
+        "Tormenta20.jpg"
+      ],
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,ico}"]
+      },
+      manifest: {
+        name: 'Mitos RPG',
+        short_name: 'MitosRPG',
+        theme_color: '#ff0000',
+        icons: [
+          {
+            src: 'd20.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'd20.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
